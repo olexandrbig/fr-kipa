02ec193d03f5979ec369f0b496f54eed3469ec0a
Oleksandr Dmukhovskyi <olexandrbig@gmail.com>
2022-11-03 21:17:29 +0200

    test build

--- docs/static/1667245918/fr-kipa/add/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/fr-kipa/add", {data:[{}],fetch:{},mutations:[]});
--- docs/static/1667245918/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Ffr-kipa\u002Fadd","\u002F","\u002Fadd","\u002Ffr-kipa\u002Fedit","\u002Fedit","\u002Ffr-kipa"]})
