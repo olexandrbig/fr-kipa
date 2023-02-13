f1724d1458c89e143960cdd18b6b55d3ea6d05bd
Oleksandr Dmukhovskyi <olexandrbig@gmail.com>
2023-02-13 23:19:50 +0200

    * Reorder in sub lvl's * Drop between levels

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,13 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 
+## 0.13.2 - 2023-02-13
+
+### Added
+* Reorder in sub lvl's
+* Drop between levels
+
+
 ## 0.13.1 - 2023-02-11
 
 ### Changed
---
+++ docs/static/1676323089/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"https:\u002F\u002Folexandrbig.github.io\u002Ffr-kipa\u002Fstatic\u002F1676323089",layout:"default",error:a,state:a,serverRendered:false,routePath:b,config:{_app:{basePath:"\u002Ffr-kipa\u002F",assetsPath:b,cdnURL:"https:\u002F\u002Folexandrbig.github.io\u002Ffr-kipa\u002F"},content:{dbHash:"f9816b3a"}}}}(null,"\u002F"));
